58e5e41370402ce314a5c0449d86c8404e214bf1
totomo <bum4496@naver.com>
2020-05-28 18:36:56 +0900

    권한 관리

--- src/main/java/Sogong/IMS/dao/AuthorityGroupDAO.java
+++ src/main/java/Sogong/IMS/dao/AuthorityGroupDAO.java
@@ -1,5 +1,15 @@
 package Sogong.IMS.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import Sogong.IMS.model.AuthorityGroup;
+
 public class AuthorityGroupDAO {
-    
+
+    public boolean lookup() {
+        return false;
+    }
 }
