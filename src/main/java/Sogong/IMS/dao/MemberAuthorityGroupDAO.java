e7ca810c32311906afdb462ba72a4aafc3ed34d7
totomo <bum4496@naver.com>
2020-05-29 00:37:51 +0900

    회원 정보 관리 추가

--- src/main/java/Sogong/IMS/dao/MemberAuthorityGroupDAO.java
+++ src/main/java/Sogong/IMS/dao/MemberAuthorityGroupDAO.java
@@ -1,25 +1,102 @@
 package Sogong.IMS.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 
 import Sogong.IMS.model.Member;
 import Sogong.IMS.model.MemberAuthorityGroup;
 
 public class MemberAuthorityGroupDAO {
 
-    // Singleton - lazy holder
-    private static class LazyHolder{
-        private static final MemberAuthorityGroupDAO dao = new MemberAuthorityGroupDAO();
-    }
-    public static MemberAuthorityGroupDAO getInstance(){
-        return LazyHolder.dao;
-    }
-    
-    public boolean enroll(MemberAuthorityGroup memberAuthorityGroup){
+    public boolean enroll(MemberAuthorityGroup memberAuthorityGroup) {
+
+        try {
+            Connection conn = null;
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            String url = "jdbc:mysql://totomo.iptime.org:3306/sogongdo?serverTimezone=UTC&zeroDateTimeBehavior=convertToNull";
+            String user = "admin";
+            String password = "k^rEmQwG2c6=bvPC";
+
+            conn = DriverManager.getConnection(url, user, password);
+        
+            PreparedStatement stmt = conn.prepareStatement("INSERT INTO `memberauthoritygroup` VALUES (?,?)");
+            stmt.setString(1, memberAuthorityGroup.getMemberID());
+            stmt.setNull(2, memberAuthorityGroup.getAuthorityGroup().getAuthorityGroupSequence());
+
+            stmt.execute();
+
+            return true;
+        
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
         return false;
     }
 
     public Member[] lookup(HashMap<String, String> condition){
+
+        try {
+            Connection conn = null;
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            String url = "jdbc:mysql://totomo.iptime.org:3306/sogongdo?serverTimezone=UTC&zeroDateTimeBehavior=convertToNull";
+            String user = "admin";
+            String password = "k^rEmQwG2c6=bvPC";
+
+            conn = DriverManager.getConnection(url, user, password);
+
+            StringBuilder builder = new StringBuilder();
+
+            builder
+                .append("SELECT * FROM ")
+                .append("memberauthoritygroup ");
+
+            if(condition != null){
+                builder.append("WHERE ");
+
+                Iterator<String> iter = condition.keySet().iterator();
+
+                while(iter.hasNext()){
+                    String key = iter.next();
+                    builder.append(String.format("`%s`=%s ", key,condition.get(key)));
+    
+                    if(iter.hasNext())
+                        builder.append("AND ");
+                }
+            }
+            
+            String sql = builder.toString();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+
+            ArrayList<Member> members = new ArrayList<>();
+
+            while(rs.next()){
+
+            }
+
+            return members.toArray(new Member[members.size()]);
+        
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
         return null;
     }
 
@@ -33,5 +110,37 @@ public class MemberAuthorityGroupDAO {
 
     public boolean delete(MemberAuthorityGroup memberAuthorityGroup){
         return false;
+    }
+
+    public static void main(String[] args){
+        // Member[] members = new MemberAuthorityGroupDAO().lookup(null);
+
+        HashMap<String, String> condition = new HashMap<String,String>();
+
+        condition.put("memberID", "totomo");
+        condition.put("memberPW", "hello");
+
+
+        StringBuilder builder = new StringBuilder();
+
+        builder
+            .append("SELECT * FROM ")
+            .append("memberauthoritygroup ");
+
+        if(condition != null){
+            builder.append("WHERE ");
+
+            Iterator<String> iter = condition.keySet().iterator();
+
+            while(iter.hasNext()){
+                String key = iter.next();
+                builder.append(String.format("`%s`=`%s` ", key,condition.get(key)));
+
+                if(iter.hasNext())
+                    builder.append("AND ");
+            }
+        }
+
+        System.out.println(builder.toString());
     }
 }
---
+++ src/main/java/Sogong/IMS/dao/MemberDAO.java
@@ -0,0 +1,27 @@
+package Sogong.IMS.dao;
+
+import java.util.HashMap;
+
+import Sogong.IMS.model.Member;
+
+public class MemberDAO {
+	boolean enroll(Member m) {
+		
+	}
+	
+	public Member[] lookup(HashMap<String, String> condition) {
+		
+	}
+	
+	boolean delete(Member m) {
+		
+	}
+	
+	boolean modify(Member m) {
+		
+	}
+	
+	public Member login(String id, String pw) {
+		
+	}
+}
