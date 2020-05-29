5bcccd9764704634318ae902138a3e9c27e8d0c7
KimDonghyeok <qoophon@gmail.com>
2020-05-29 20:32:28 +0900

    [생성] ChargeDsicountRateEnrollAction컨트롤클래스생성 및메소드구현 [생성] ChargeDsicountRateModifyAction컨트롤클래스생성 및메소드구현 [생성] ChargeDsicountRateDeleteAction컨트롤클래스생성 및메소드구현 [생성] ChargeDsicountRateLookupAction컨트롤클래스생성 및메소드구현

---
+++ src/main/java/Sogong/IMS/controller/FacilityChargeManagement/ChargeDiscountRateLookupAction.java
@@ -0,0 +1,14 @@
+package Sogong.IMS.controller.FacilityChargeManagement;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Sogong.IMS.controller.Action;
+
+public class ChargeDiscountRateLookupAction implements Action {
+    @Override
+    public void excute(HttpServletRequest request, HttpServletResponse response) {
+        // TODO Auto-generated method stub
+        
+    }
+}
