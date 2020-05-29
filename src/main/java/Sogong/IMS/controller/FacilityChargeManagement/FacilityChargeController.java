5b42adfadafd2557df5245c3934c72625715f688
KimDonghyeok <qoophon@gmail.com>
2020-05-29 20:56:27 +0900

    [수정] 오류 수정, Sogong.IMS.model.Member import

--- src/main/java/Sogong/IMS/controller/FacilityChargeManagement/FacilityChargeController.java
+++ src/main/java/Sogong/IMS/controller/FacilityChargeManagement/FacilityChargeController.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import Sogong.IMS.controller.Action;
+import Sogong.IMS.model.Member;
 
 @WebServlet("/facilityChargeMangeView")
 public class FacilityChargeController extends HttpServlet {
