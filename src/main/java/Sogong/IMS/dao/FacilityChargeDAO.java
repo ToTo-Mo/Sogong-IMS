64a79a9644dc4897a3ef8dba5907fbaea0d1300e
KimDonghyeok <qoophon@gmail.com>
2020-05-31 15:03:34 +0900

    [수정] FacilityChargeDAO enroll 메소드 작성

--- src/main/java/Sogong/IMS/dao/FacilityChargeDAO.java
+++ src/main/java/Sogong/IMS/dao/FacilityChargeDAO.java
@@ -1,11 +1,48 @@
 package Sogong.IMS.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.HashMap;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 import Sogong.IMS.model.FacilityCharge;
 
 public class FacilityChargeDAO {
     public boolean enroll(FacilityCharge facilityCharge) {
+        try {
+
+            // DB연결
+            Connection conn = null;
+            PreparedStatement stmt = null;
+
+            // META-INF아래 context.xml
+            Context context  = new InitialContext();
+            // DB Connection
+            conn = ((DataSource) context.lookup("java:comp/env/jdbc/mysql")).getConnection();
+
+            String sql = "INSERT INTO `facilitycharge`(`facilityID`, `chargeName`, `charge`, `resistrantID`) VALUES (?,?,?,)";
+            stmt = conn.prepareStatement(sql);
+
+            // 시설이름, 요금명, 요금, 등록자를 Value에 각각 지정
+            stmt.setString(1, facilityCharge.getFacilityID());
+            stmt.setString(2, facilityCharge.getChargeName());
+            stmt.setInt(3, facilityCharge.getCharge());
+            stmt.setString(4, facilityCharge.getResistrantID());
+
+            stmt.execute();
+
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+
         return false;
     }
 
