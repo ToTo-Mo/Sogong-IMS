f92b3795c1f3016df7d4496cf1cdee77c546e81c
dongky9797 <sun00125@naver.com>
2020-06-02 18:22:31 +0900

    0602

--- src/main/java/Sogong/IMS/controller/AccomodationBookManagement/AccomodationBookManagementController.java
+++ src/main/java/Sogong/IMS/controller/AccomodationBookManagement/AccomodationBookManagementController.java
@@ -1,7 +1,38 @@
 package Sogong.IMS.controller.AccomodationBookManagement;
 
-import javax.servlet.http.HttpServlet;
+import java.util.HashMap;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Sogong.IMS.controller.Action;
 public class AccomodationBookManagementController extends HttpServlet {
-    
+    //private static final long serialVersionUID = 8559171819500212874L;
+
+    HashMap<String, Action> list = null;
+
+    @Override
+    public void init(ServletConfig sc) throws ServletException {
+        list = new HashMap<>();
+
+        list.put("/enroll.do", new AccomodationBookHistoryEnrollAction());
+        list.put("/lookup.do", new AccomodationBookHistoryLookupAction());
+        list.put("/modify.do", new AccomodationBookHistoryModifyAction());
+        list.put("/delete.do", new AccomodationBookHistoryDeleteAction());
+    }
+
+    // get이나 post 요청에 대한 처리를 수행합니다.
+    @Override
+    public void service(HttpServletRequest request, HttpServletResponse response) {
+        String url = request.getRequestURI();
+        String servletPath = request.getServletPath();
+
+        String path = url.substring(servletPath.length());
+
+        Action action = list.get(path);
+        action.excute(request, response);
+    }
 }
