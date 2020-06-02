180f498c06cb836af72a26e5c9db813b6669799d
totomo <bum4496@naver.com>
2020-06-02 19:56:13 +0900

    시설 관리 : 구현중

---
+++ src/main/java/Sogong/IMS/model/FacilityProperty.java
@@ -0,0 +1,16 @@
+package Sogong.IMS.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class FacilityProperty {
+    String facilityPropertyID;
+    String facilityID;
+    String registrantID;
+    //DATE openingDate;
+    int facilityScale;
+    int facilityCost;
+    
+}
