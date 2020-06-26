0e29a956ba7a9e81249fe2fd4f7f9f2608904259
totomo <bum4496@naver.com>
2020-06-26 11:30:36 +0900

    시설 요금 관리 : 권한 확인 완료

--- src/main/java/Sogong/IMS/controller/FacilityChargeManagement/FacilityChargeController.java
+++ src/main/java/Sogong/IMS/controller/FacilityChargeManagement/FacilityChargeController.java
@@ -1,5 +1,6 @@
 package Sogong.IMS.controller.FacilityChargeManagement;
 
+import java.io.PrintWriter;
 import java.util.HashMap;
 
 import javax.servlet.ServletConfig;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import Sogong.IMS.controller.Action;
+import Sogong.IMS.dao.MemberAuthorityGroupDAO;
 import Sogong.IMS.model.Member;
 import lombok.SneakyThrows;
 
@@ -32,16 +34,30 @@ public class FacilityChargeController extends HttpServlet {
 
     @SneakyThrows
     @Override
-    public void service(HttpServletRequest request, HttpServletResponse response){
+    public void service(HttpServletRequest request, HttpServletResponse response) {
         String url = request.getRequestURI();
         String servletPath = request.getServletPath();
         String path = url.substring(servletPath.length()).split("/")[1];
 
-        Action action = list.get(path);
-        action.excute(request, response);
+        PrintWriter out = response.getWriter();
+
+        HashMap<String, String> authorityList = new HashMap<>();
+        authorityList.put("enroll.do", "시설요금_등록");
+        authorityList.put("modify.do", "시설요금_수정");
+        authorityList.put("delete.do", "시설요금_삭제");
+        authorityList.put("lookup.do", "시설요금_조회");
+
+        if (request.getSession().getAttribute("member") != null
+                && hasAuthority((Member) request.getSession().getAttribute("member"), authorityList.get(path))) {
+            Action action = list.get(path);
+            action.excute(request, response);
+        } else {
+            out.print(String.format("<script>alert('권한이 없습니다'); location.replace('%s');</script>",
+                    request.getServletPath()));
+        }
     }
 
-    public boolean hasAuthority(Member member, String authorityName){
-        return false;
+    public boolean hasAuthority(Member member, String authorityName) {
+        return new MemberAuthorityGroupDAO().hasAuthority(member, authorityName);
     }
 }
